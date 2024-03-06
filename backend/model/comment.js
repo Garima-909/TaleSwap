dfe7fd3916811808f17531ba10b82bd23f510d41
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-03-06 21:38:18 +0530

    create comment schema

---
+++ backend/model/comment.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+    },
+    content: {
+        type: String,
+        maxLength: 1000,
+    },
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
